aa813175419a29bff00bf63067c6c0baaa5ecee2
alimovscott <abduqodiralimov23@icloud.com>
2026-05-17 19:17:23 +0900

    feat: develop property related comments data

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -27,12 +27,13 @@ import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { GET_PROPERTIES, GET_PROPERTY } from '../../apollo/user/query';
+import { GET_COMMENTS, GET_PROPERTIES, GET_PROPERTY } from '../../apollo/user/query';
 import { T } from '../../libs/types/common';
 import { dir } from 'console';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
 import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
+import { PropertyLocation } from '../../libs/enums/property.enum';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -96,7 +97,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 					sort:'createdAt',
 					direction: Direction.DESC,
 					search: {
-						locationList: [property?.propertyLocation],
+						locationList: property?.propertyLocation ? [property?.propertyLocation] : [],
 					}
 				} 
 			},
@@ -106,6 +107,25 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 				if(data?.getProperties?.list) setDestinationProperties(data?.getProperties?.list);
 			},
 		});
+
+
+		const {
+			loading: getCommentsLoading,
+			data: getCommentsData,
+			error: getCommentsError,
+			refetch: getCommentsRefetch,
+		} = useQuery(GET_COMMENTS, {
+			fetchPolicy: 'cache-and-network',
+			variables: {
+	input: initialComment
+},
+			skip:!commentInquiry.search.commentRefId,
+			notifyOnNetworkStatusChange: true,
+			onCompleted: (data: T) => {
+				if(data?.getComments?.list) setPropertyComments(data?.getComments?.list);
+				setCommentTotal(data?.getComments?.metaCounter[0].total ?? 0);
+			},
+		});	
 
 	/** LIFECYCLES **/
 	useEffect(() => {
@@ -124,7 +144,14 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 		}
 	}, [router]);
 
-	useEffect(() => {}, [commentInquiry]);
+	useEffect(() => {
+		if (commentInquiry.search.commentRefId){
+			getCommentsRefetch({ input: { ...commentInquiry } });
+		}
+		
+	}, 
+	
+	[commentInquiry]);
 
 	/** HANDLERS **/
 	const changeImageHandler = (image: string) => {
