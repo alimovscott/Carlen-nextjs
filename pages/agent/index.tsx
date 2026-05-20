94a08c708ea302cc7a60f62de0715c26c2db7239
alimovscott <abduqodiralimov23@icloud.com>
2026-05-20 15:12:39 +0900

    feat: develop agents list screen component logic

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -9,6 +9,12 @@ import AgentCard from '../../libs/components/common/AgentCard';
 import { useRouter } from 'next/router';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Member } from '../../libs/types/member/member';
+import { useMutation, useQuery } from '@apollo/client';
+import { LIKE_TARGET_MEMBER } from '../../apollo/user/mutation';
+import { GET_AGENTS } from '../../apollo/user/query';
+import { T } from '../../libs/types/common';
+import { Message } from '../../libs/enums/common.enum';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 export const getStaticProps = async ({ locale }: any) => ({
 	props: {
@@ -32,7 +38,27 @@ const AgentList: NextPage = ({ initialInput, ...props }: any) => {
 	const [searchText, setSearchText] = useState<string>('');
 
 	/** APOLLO REQUESTS **/
+	const [likeTargetMember] = useMutation(LIKE_TARGET_MEMBER);
+
+	const {
+		loading: getAgentsLoading,
+		data: getAgentsData,
+		error: getAgentsError,
+		refetch: getAgentsRefetch,
+	} = useQuery(GET_AGENTS, {
+		fetchPolicy: 'network-only',
+		variables: {input: searchFilter},
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data : T) => {
+			setAgents(data?.getAgents?.list);
+			setTotal(data?.getAgents?.metaCounter[0]?.total);
+		},
+	}
+)
+
+
 	/** LIFECYCLES **/
+	
 	useEffect(() => {
 		if (router.query.input) {
 			const input_obj = JSON.parse(router?.query?.input as string);
@@ -84,6 +110,27 @@ const AgentList: NextPage = ({ initialInput, ...props }: any) => {
 		});
 		setCurrentPage(value);
 	};
+
+
+	const likeMemberHandler = async (user: T, id: string) => {
+			try {
+				if (!id) return;
+				if (!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+	
+				await likeTargetMember({ 
+					variables: { input: id, 
+
+					},
+				 });
+	
+				await getAgentsRefetch({ input: searchFilter });
+				await sweetTopSmallSuccessAlert('success', 800);
+			} catch (err: any) {
+				console.log('ERROR, likeMemberHandler:', err.message);
+				sweetMixinErrorAlert(err.message).then();
+			}
+		};
+
 
 	if (device === 'mobile') {
 		return <h1>AGENTS PAGE MOBILE</h1>;
@@ -139,7 +186,7 @@ const AgentList: NextPage = ({ initialInput, ...props }: any) => {
 							</div>
 						) : (
 							agents.map((agent: Member) => {
-								return <AgentCard agent={agent} key={agent._id} />;
+								return <AgentCard agent={agent} key={agent._id} likeMemberHandler={likeMemberHandler} />;
 							})
 						)}
 					</Stack>
@@ -181,3 +228,11 @@ AgentList.defaultProps = {
 };
 
 export default withLayoutBasic(AgentList);
+function likeTargetProperty(arg0: { variables: { input: string; }; }) {
+	throw new Error('Function not implemented.');
+}
+
+function getPropertiesRefetch(arg0: { input: any; }) {
+	throw new Error('Function not implemented.');
+}
+
